1527e1ee497d571e9a6ae15335946a6d80c5c3cc
KirillGorb <kirillgor12506@gmail.com>
2023-07-13 08:40:54 +0300

    DopMethot

--- Practic/Practic.cpp
+++ Practic/Practic.cpp
@@ -1,18 +1,26 @@
 ﻿#include <iostream>;
 #include <cmath>;
+#include <iomanip>;
 
 using namespace std;
 
 double Function(double x) {
-	return x * x - 4;
+	return cos(x) + sin(log10(x));
 }
 double FunctionPr(double x) {
-	return 2 * x;
+	return cos(log10(x)) / (log(10) * x) * 1 / x - sin(x);
+}
+
+double Function2(double x) {
+	return 8 * x * sin(x) - log(x);
+}
+double FunctionPr2(double x) {
+	return 8 * (sin(x) + x * cos(x)) - 1 / x;
 }
 
 double Bisection(double epsilon) {
-	double a = 1;
-	double b = 3;
+	double a = 1.75;
+	double b = 2;
 
 	if (Function(a) * Function(b) >= 0) {
 		cout << "Ошибка: функция не удовлетворяет условию f(a) * f(b) < 0" << endl;
@@ -38,7 +46,7 @@ double Bisection(double epsilon) {
 
 double Newton(double epsilon, int maxIterations) {
 	int iteration = 0;
-	double x = 2;
+	double x = 1.75;
 
 	while (abs(Function(x)) >= epsilon && iteration < maxIterations) {
 		x = x - Function(x) / FunctionPr(x);
@@ -48,7 +56,9 @@ double Newton(double epsilon, int maxIterations) {
 	return x;
 }
 double Secant(double epsilon, int maxIterations) {
-	double x0 = 1; double x1 = 2;
+	double x0 = 1.75;
+	double x1 = 2;
+
 	int iteration = 0;
 	double x = x1;
 
@@ -72,7 +82,8 @@ double Secant(double epsilon, int maxIterations) {
 
 double Find(float eps)
 {
-	double a = 1, b = 10, x;
+	double a = 1.75;
+	double b = 2, x;
 
 	while (abs(a - b) > eps) {
 		x = (a + b) / 2;
@@ -88,12 +99,58 @@ double Find(float eps)
 
 void main() {
 
+	system("color F0");
+
 	setlocale(LC_ALL, "rus");
 
 	double e = 0.0001;
 
+	cout << setprecision(52);
 	cout << Bisection(e) << "\n\n";
 	cout << Newton(e, 2000) << "\n\n";
 	cout << Secant(e, 2000) << "\n\n";
 	cout << Find(e) << "\n\n";
+
+	//////////////////////////////////
+	int arr[20];
+	int x = 1;
+	int j = 0;
+	int i = 2;
+	while (true)
+	{
+		if (x % 2 == 0)
+			x /= 2;
+		else
+			x = 3 * x + 1;
+
+		if (x == 8848) {
+			arr[j++] = i;
+			cout << i << "\n";
+			x = i++;
+			if (j > 3)
+				break;
+		}
+		if (x > 8848 || x <= 1) {
+			x = i++;
+			if (j > 3)
+				break;
+		}
+	}
+
+	int k;
+	cin >> k;
+	k--;
+	if (k < 20 && k >= 0)
+		cout << arr[k];
+	else
+		cout << -1;
+	/////////////////////////////////////////
+
+	int a, b;
+	cin >> a >> b;
+	float s = (a + b * sqrt(2));
+	float rez = sqrt(s/ (2+2*sqrt(2)));
+	if (rez == (int)rez)
+		cout << 1;
+	else cout << 0;
 }
